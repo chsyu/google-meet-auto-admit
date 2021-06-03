4514a2905512b46d4fd16c4037b1fb9916bb55f9
Chi-Shan Yu <chisamyu@seed.net.tw>
2021-06-03 08:46:22 +0800

    移除console.log

--- content.js
+++ content.js
@@ -2,11 +2,9 @@
 function autoAdmit() {
    for (let element of document.getElementsByTagName('span')) {
        if (element.innerHTML === '接受') {
-           console.log('There is someone waiting to join this meeting, automatically admitting them...');
            element.click();
        }
    }
 }
 
-console.log('Enabling Auto Admit');
 setInterval(autoAdmit, 1000);
